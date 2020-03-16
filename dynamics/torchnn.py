fee3f4826ecffc865db4eaffde163c93dfb42174
Andrew <b06901160@ntu.edu.tw>
2020-03-16 21:46:12 +0800

    New device

--- dynamics/torchnn.py
+++ dynamics/torchnn.py
@@ -1,54 +1,118 @@
 # -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
 import torch
+
+
+dtype = torch.float
+# device = torch.device("cpu")
+device = torch.device("cuda:0") # Uncomment this to run on GPU
 
 # N is batch size; D_in is input dimension;
 # H is hidden dimension; D_out is output dimension.
 N, D_in, H, D_out = 64, 1000, 100, 10
 
-# Create random Tensors to hold inputs and outputs
-x = torch.randn(N, D_in)
-y = torch.randn(N, D_out)
+# y = w2 * w1 * x
+y_size, x_size, mid_dim = 100, 100, 1000
 
-# Use the nn package to define our model as a sequence of layers. nn.Sequential
-# is a Module which contains other Modules, and applies them in sequence to
-# produce its output. Each Linear Module computes output from input using a
-# linear function, and holds internal Tensors for its weight and bias.
-model = torch.nn.Sequential(
-    torch.nn.Linear(D_in, H),
-    torch.nn.ReLU(),
-    torch.nn.Linear(H, D_out),
-)
+# Create random input and output data
+x = torch.randn(x_size, 1, device=device, dtype=dtype)
+y = torch.randn(y_size, 1, device=device, dtype=dtype)
 
-# The nn package also contains definitions of popular loss functions; in this
-# case we will use Mean Squared Error (MSE) as our loss function.
-loss_fn = torch.nn.MSELoss(reduction='sum')
+# Randomly initialize weights
+w1 = torch.randn(mid_dim, x_size, device=device, dtype=dtype)
+w2 = torch.randn(y_size, mid_dim, device=device, dtype=dtype)
 
-learning_rate = 1e-4
-for t in range(500):
-    # Forward pass: compute predicted y by passing x to the model. Module objects
-    # override the __call__ operator so you can call them like functions. When
-    # doing so you pass a Tensor of input data to the Module and it produces
-    # a Tensor of output data.
-    y_pred = model(x)
+# Input correlation 
 
-    # Compute and print loss. We pass Tensors containing the predicted and true
-    # values of y, and the loss function returns a Tensor containing the
-    # loss.
-    loss = loss_fn(y_pred, y)
-    if t % 100 == 99:
-        print(t, loss.item())
+# Input correlation 
+input_correlation = x.mm(x.t())
 
-    # Zero the gradients before running the backward pass.
-    model.zero_grad()
+input_output_correlation = y.mm(x.t())
 
-    # Backward pass: compute gradient of the loss with respect to all the learnable
-    # parameters of the model. Internally, the parameters of each Module are stored
-    # in Tensors with requires_grad=True, so this call will compute gradients for
-    # all learnable parameters in the model.
-    loss.backward()
+learning_rate = 1e-8
 
-    # Update the weights using gradient descent. Each parameter is a Tensor, so
-    # we can access its gradients like we did before.
-    with torch.no_grad():
-        for param in model.parameters():
-            param -= learning_rate * param.grad
+loss_list = []
+p_difference_list = []
+
+
+epoch = int(5E4)
+
+for t in range(epoch):
+	# Forward pass: compute predicted y
+	h = w1.mm(x)
+	# h_relu = np.maximum(h, 0)
+	y_pred = w2.mm(h)
+
+	# Compute and print loss
+	loss = (y_pred - y).pow(2).sum().item()
+	print(t, "loss = ",loss)
+
+	# Backprop to compute gradients of w1 and w2 with respect to loss
+	grad_y_pred = 2.0 * (y_pred - y)
+	# grad_w2 = h.t().mm(grad_y_pred)
+	grad_w2 = grad_y_pred.mm(h.t())
+	inter = w2.t().mm(grad_y_pred)
+	grad_w1 = inter.mm(x.t())
+
+
+	subtraction_term = input_output_correlation - (w2.mm(w1.mm(input_correlation)))
+	w1_right = learning_rate * w2.t().mm(subtraction_term)
+	w2_right = learning_rate * subtraction_term.mm(w1.t())
+
+	# P1 = grad_w1.mm(np.linalg.inv(w1_right))
+	# P2 = grad_w2.mm(np.linalg.inv(w2_right))
+	p1 = grad_w1[1][1] / w1_right[1][1] / 2
+	p2 = grad_w2[1][1] / w2_right[1][1] / 2
+	# print ("P1 value = ",p1)
+	# print ("P2 value = ",p2)
+	p_difference = p1.norm() - p2.norm()
+	print ("P difference = ",p_difference)
+	p_difference_list.append(p_difference)
+	loss_list.append(loss)
+	# ratio = P * learning_rate 
+	# print ("ratio = ",ratio)
+	# P1 = np.divide(grad_w1,w1_right)
+	# P2 = np.divide(grad_w2,w2_right)
+
+	# p1_norm = np.linalg.norm(P1)
+	# p2_norm = np.linalg.norm(P2)
+
+	# difference = p1_norm - p2_norm 
+
+	# print ("difference = ",difference)
+
+	# print ("P1, P2 = ",P1,P2)
+	
+
+
+
+	# Update weights
+	w1 -= learning_rate * grad_w1
+	w2 -= learning_rate * grad_w2
+
+
+
+
+
+
+x_axis = list(range(epoch))
+# plt.plot(x_axis,p_difference_list,label='p_difference')
+plt.plot(x_axis,loss_list, label='loss')
+# plt.yscale('log')
+# plt.savefig('100_depth_weight_mean_without_xavier_linear_activation')
+plt.legend(loc='upper left')
+plt.yscale('log')
+# plt.show()
+plt.savefig('learning_rate_%s_learning_dynamics_difference' % learning_rate)
+# plt.savefig()
+
+
+
+
+
+
+
+
+
+
